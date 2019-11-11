125003d0807d8b385c5ab72cbac87a99949999a5
Arnaud Doucerain <adoucerain@octo.com>
2019-11-11 23:18:43 +0100

    Prevent a user from voting twice neither for the same answer nor for the same question

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,15 @@
+class VoteValidator < ActiveModel::Validator
+  def validate(vote)
+    if Vote.exists?(user_id: vote.user_id, answer_id: vote.answer_id)
+      vote.errors[:user_id] << 'should vote for an answer once'
+    elsif Vote.exists?(user_id: vote.user_id, answer: Answer.where(question_id: vote.answer&.question_id))
+      vote.errors[:user_id] << 'should vote for a question once'
+    end
+  end
+end
+
 class Vote < ApplicationRecord
   belongs_to :answer
+
+  validates_with VoteValidator
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -6,4 +6,36 @@ RSpec.describe Vote, type: :model do
 
     it { expect(vote).to belong_to :answer }
   end
+
+  describe 'validations' do
+    describe 'a user cannot vote twice neither for the same answer nor for the same question' do
+      let(:question) { create :question }
+      let(:first_answer) { create :answer, question: question }
+
+      context 'current_user has already voted for this answer' do
+        let!(:previous_vote) { create :vote, user_id: current_user['id'], answer: first_answer }
+
+        let(:new_vote) { build :vote, user_id: current_user['id'], answer: first_answer }
+
+        it { expect(new_vote.valid?).to be false }
+      end
+
+      context 'current_user has already voted for this question' do
+        let(:second_answer) { create :answer, question: question }
+        let!(:previous_vote) { create :vote, user_id: current_user['id'], answer: first_answer }
+
+        let(:new_vote) { build :vote, user_id: current_user['id'], answer: second_answer }
+
+        it { expect(new_vote.valid?).to be false }
+      end
+
+      context 'current_user has not already voted for this question' do
+        let!(:previous_vote) { create :vote, user_id: current_user['id'] }
+
+        let(:new_vote) { build :vote, user_id: current_user['id'], answer: first_answer }
+
+        it { expect(new_vote.valid?).to be true }
+      end
+    end
+  end
 end
