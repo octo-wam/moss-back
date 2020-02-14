79fcb2a23e7e2274f56df47774c1578b34be2626
ALME <mehdi.al.ouafi@octo.com>
2020-02-14 15:21:41 +0100

    feat(send-mail-to-specific-mailing-list): new attribute for treating recipient

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -14,20 +14,23 @@ module Api
       def create
         @question = Question.new(question_params)
         raise ActionController::BadRequest, 'Answers should be filled' if @question.answers.empty?
-
         @question.save!
-        NotificationMailer.with(question: @question).new_question.deliver_later
-
+        NotificationMailer.with(question: @question).new_question(recipient_param).deliver_later
         render status: :created
       end
 
       private
 
       def question_params
-        new_params = params.permit(:title, :description, :endingDate, answers: %i[title description])
-        new_params[:ending_date] = new_params.delete :endingDate if params[:endingDate]
-        new_params[:answers_attributes] = new_params.delete :answers if params[:answers]
+        new_params = params[:question].permit(:title, :description, :endingDate, answers: %i[id title description])
+        new_params[:ending_date] = new_params.delete :endingDate if params[:question][:endingDate]
+        new_params[:answers_attributes] = new_params.delete :answers if params[:question][:answers]
         new_params
+      end
+
+      def recipient_param
+        new_param = params[:recipients]
+        new_param
       end
     end
   end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class NotificationMailer < ApplicationMailer
-  def new_question
+  def new_question(recipients)
     @question = params[:question]
     @question_url = "#{ENV['FRONT_BASE_URL']}/question/#{@question.id}"
-    mail(to: ENV['TEAM_EMAIL_ADDRESS'], subject: '[WAM] Nous avons besoin de ton avis !')
+    finalRecipients = "#{ENV['TEAM_EMAIL_ADDRESS']}#{',' + recipients.join(',') unless recipients.length === 0}"
+
+    mail(to: finalRecipients, subject: '[WAM] Nous avons besoin de ton avis !')
   end
 end
