a67d4c82e2c25cb6afc9b4f8ecc3c60318f6f325
Arnaud Doucerain <adoucerain@octo.com>
2020-03-18 02:16:37 +0100

    Possibility to sort a question on date and title

--- spec/requests/api/v1/questions_request_spec.rb
+++ spec/requests/api/v1/questions_request_spec.rb
@@ -7,25 +7,88 @@ describe 'Questions', type: :request do
   let(:question) { create :question, user: user }
 
   describe 'GET /v1/questions' do
-    let!(:answer) { create :answer, question: question }
+    describe 'simple request' do
+      let!(:answer) { create :answer, question: question }
 
-    before do
-      get '/api/v1/questions', headers: headers_of_logged_in_user
+      before do
+        get '/api/v1/questions',  headers: headers_of_logged_in_user
+      end
+
+      it 'returns an ok HTTP status' do
+        expect(response).to have_http_status :ok
+      end
+
+      it 'returns an array with one question containing one answer' do
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body.size).to eq(1)
+        expect(parsed_body.first['title']).to eq(question.title)
+        expect(parsed_body.first['answers'].size).to eq(1)
+        expect(parsed_body.first['answers'].first['title']).to eq(answer.title)
+        expect(parsed_body.first['user']).to eq('id' => question.user_id,
+                                                'name' => user.name,
+                                                'photo' => user.photo)
+      end
     end
 
-    it 'returns an ok HTTP status' do
-      expect(response).to have_http_status :ok
+    context 'request with sort=created_at parameter' do
+      let!(:params) { { sort: 'created_at' } }
+
+      let!(:recent_question) { create :question, created_at: 1.hour.ago }
+      let!(:old_question) { create :question, created_at: 1.year.ago }
+
+      before do
+        question.update created_at: 1.day.ago
+
+        get '/api/v1/questions', params: params, headers: headers_of_logged_in_user
+      end
+
+      it 'returns an array with three questions sorted by date asc' do
+        parsed_body = JSON.parse(response.body)
+        question_ids = parsed_body.map { |q| q['id'] }
+
+        expect(question_ids).to eq([old_question.id, question.id, recent_question.id])
+      end
     end
 
-    it 'returns an array with one question containing one answer' do
-      parsed_body = JSON.parse(response.body)
-      expect(parsed_body.size).to eq(1)
-      expect(parsed_body.first['title']).to eq(question.title)
-      expect(parsed_body.first['answers'].size).to eq(1)
-      expect(parsed_body.first['answers'].first['title']).to eq(answer.title)
-      expect(parsed_body.first['user']).to eq('id' => question.user_id,
-                                              'name' => user.name,
-                                              'photo' => user.photo)
+    context 'request with sort=created_at:desc parameter' do
+      let!(:params) { { sort: 'created_at:desc' } }
+
+      let!(:recent_question) { create :question, created_at: 1.hour.ago }
+      let!(:old_question) { create :question, created_at: 1.year.ago }
+
+      before do
+        question.update created_at: 1.day.ago
+
+        get '/api/v1/questions', params: params, headers: headers_of_logged_in_user
+      end
+
+      it 'returns an array with three questions sorted by date desc' do
+        parsed_body = JSON.parse(response.body)
+        question_ids = parsed_body.map { |q| q['id'] }
+
+        expect(question_ids).to eq([recent_question.id, question.id, old_question.id])
+      end
+    end
+
+    context 'request with sort=created_at:desc,title parameter' do
+      let!(:params) { { sort: 'created_at:desc,title' } }
+
+      let(:identical_date) { 1.hour.ago.to_date }
+      let!(:question_starting_with_w) { create :question, title: 'What is the main purpose of a lockdown?', created_at: identical_date }
+      let!(:question_starting_with_o) { create :question, title: 'On which date will we be allowed to go out?', created_at: identical_date }
+
+      before do
+        question.update created_at: 1.day.ago
+
+        get '/api/v1/questions', params: params, headers: headers_of_logged_in_user
+      end
+
+      it 'returns an array with three questions sorted by date desc, then by title asc' do
+        parsed_body = JSON.parse(response.body)
+        question_ids = parsed_body.map { |q| q['id'] }
+
+        expect(question_ids).to eq([question_starting_with_o.id, question_starting_with_w.id, question.id])
+      end
     end
   end
 
