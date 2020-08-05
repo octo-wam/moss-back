bfa35e0af3d4e1bae01247cd7b843b2cdd18d5bc
OctoMaieonBrixxy <60095036+OctoMaieonBrixxy@users.noreply.github.com>
2020-08-05 17:04:16 +0200

    feat: add redirection for google type auth (#11)
    * feat: add redirection for google type auth
    
    Fix #4 on [moss-front](https://github.com/octo-wam/moss-front/issues/4)
    
    * lint
    
    * renaming var
    
    * Extract redirect URL logic & Validate domain name of redirect_to parameter
    
    * Fix assertion text
    
    * fix a typo in test description
    
    Co-authored-by: Arnaud Doucerain <adoucerain@octo.com>
    Co-authored-by: Anne-Marie <amesteves@octo.com>

--- app/controllers/google_oauth_callback_controller.rb
+++ app/controllers/google_oauth_callback_controller.rb
@@ -3,10 +3,18 @@
 class GoogleOauthCallbackController < ApplicationController
   def callback
     @response = request.env['omniauth.auth']
-    redirect_to "#{ENV['FRONT_BASE_URL']}#access_token=#{token}"
+    redirect_to front_app_url_with_token(request.env)
   end
 
   private
+
+  def front_app_url_with_token(env)
+    if env['omniauth.params']['redirect_to'].starts_with? ENV['FRONT_BASE_URL']
+      front_app_url = env['omniauth.params']['redirect_to']
+    end
+    front_app_url ||= ENV['FRONT_BASE_URL']
+    "#{front_app_url}#access_token=#{token}"
+  end
 
   def validate_domain
     hosted_domain = @response['extra']['raw_info']['hd']
---
+++ spec/controllers/google_oauth_callback_controller_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe GoogleOauthCallbackController, type: :controller do
+  let(:controller) { described_class.new }
+
+  describe 'front_app_url_with_token' do
+    subject(:front_app_url) { controller.send(:front_app_url_with_token, request_env) }
+
+    before do
+      ENV['FRONT_BASE_URL'] = 'https://moss-front.fr/'
+
+      allow(controller).to receive(:token).and_return('my-access-token')
+    end
+
+    context 'URL in redirect_to query param starts with front URL' do
+      let(:request_env) do
+        {
+          'omniauth.params' => {
+            'redirect_to' => 'https://moss-front.fr/ma-page-de-redirection/'
+          }
+        }
+      end
+
+      it 'returns the URL given in redirect_to query param' do
+        expect(front_app_url).to eq 'https://moss-front.fr/ma-page-de-redirection/#access_token=my-access-token'
+      end
+    end
+
+    context 'URL in redirect_to query param does not start with front URL' do
+      let(:request_env) do
+        {
+          'omniauth.params' => {
+            'redirect_to' => 'https://mon-site-de-hack.er/'
+          }
+        }
+      end
+
+      it 'returns an URL that is not redirect_to query param' do
+        expect(front_app_url).to eq 'https://moss-front.fr/#access_token=my-access-token'
+      end
+    end
+  end
+end
