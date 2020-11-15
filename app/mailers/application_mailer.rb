4ab91486dbd92e4e5dba5e1e5036f8f8734c4f91
Arnaud Doucerain <arnaud.doucerain@octo.com>
2020-11-15 21:54:13 +0100

    Put mandatory environment variables back to hash access

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  SENDER_EMAIL = ENV.fetch('APPLICATION_EMAIL_ADDRESS')
+  SENDER_EMAIL = ENV['APPLICATION_EMAIL_ADDRESS']
   SENDER = "MOSS <#{SENDER_EMAIL}>"
   default from: SENDER
 
