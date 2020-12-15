d9aa5e24164212390e3a02f94bd348d40ae96677
Arnaud Doucerain <arnaud.doucerain@octo.com>
2020-12-15 18:45:56 +0100

    Code Review - Use upsert_user && Fix @auth_info variable that was not initialized

--- app/controllers/google_oauth_callback_controller.rb
+++ app/controllers/google_oauth_callback_controller.rb
@@ -2,14 +2,15 @@
 
 class GoogleOauthCallbackController < ApplicationController
   def callback
-    @response = request.env['omniauth.auth']
+    @google_oauth2_response = request.env['omniauth.auth']
+    upsert_user
     redirect_to front_app_url_with_token(request.env)
   end
 
   private
 
   def front_app_url_with_token(env)
-    if env['omniauth.params']['redirect_to'].starts_with? ENV['FRONT_BASE_URL']
+    if env['omniauth.params']['redirect_to'].start_with? ENV['FRONT_BASE_URL']
       front_app_url = env['omniauth.params']['redirect_to']
     end
     front_app_url ||= ENV['FRONT_BASE_URL']
@@ -17,27 +18,27 @@ class GoogleOauthCallbackController < ApplicationController
   end
 
   def validate_domain
-    hosted_domain = @auth_info['extra']['raw_info']['hd']
+    hosted_domain = @google_oauth2_response['extra']['raw_info']['hd']
     render text: 'You must be part of OCTO Technology' unless hosted_domain == 'octo.com'
   end
 
   def upsert_user
-    user = User.find_or_initialize_by(id: @auth_info['extra']['raw_info']['sub'])
+    user = User.find_or_initialize_by(id: @google_oauth2_response['extra']['raw_info']['sub'])
     user.assign_attributes(
-      name: @auth_info['info']['name'],
-      email: @auth_info['info']['email'],
-      photo: @auth_info['extra']['raw_info']['picture']
+      name: @google_oauth2_response['info']['name'],
+      email: @google_oauth2_response['info']['email'],
+      photo: @google_oauth2_response['extra']['raw_info']['picture']
     )
     user.save!
   end
 
   def token
     payload = {
-      name: @auth_info['info']['name'],
-      email: @auth_info['info']['email'],
-      photo: @auth_info['extra']['raw_info']['picture'],
-      exp: @auth_info['credentials']['expires_at'],
-      sub: @auth_info['extra']['raw_info']['sub']
+      name: @google_oauth2_response['info']['name'],
+      email: @google_oauth2_response['info']['email'],
+      photo: @google_oauth2_response['extra']['raw_info']['picture'],
+      exp: @google_oauth2_response['credentials']['expires_at'],
+      sub: @google_oauth2_response['extra']['raw_info']['sub']
     }
 
     JWT.encode payload, ENV['SECRET_KEY_BASE'], 'HS256'
